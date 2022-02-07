782f53b8b5fdbfff173bdae8a896acf2c108c9a9
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-02-07 16:56:32 -0700

    finished mvp

--- src/index.js
+++ src/index.js
@@ -41,41 +41,106 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 
 console.log('project wired!')
 
-const nav = document.querySelectorAll("nav a");
-const nav1 = document.querySelector("nav a");
-const nav2 =nav1.nextElementSibling;
-const nav3 =nav2.nextElementSibling;
-const nav4 =nav3.nextElementSibling;
-const nav5 =nav4.nextElementSibling;
-const nav6 =nav5.nextElementSibling;
 
-const logo = document.querySelector("#logo-image");
+//header 
+const nav = document.querySelectorAll('nav a');
+const nav1 = nav[0]
+const nav2 = nav[1]
+const nav3 = nav[2]
+const nav4 = nav[3]
+const nav5 = nav[4]
+const nav6 = nav[5]
+nav1.textContent = siteContent['nav']['nav-item-1'];
+nav2.textContent = siteContent['nav']['nav-item-2'];
+nav3.textContent = siteContent['nav']['nav-item-3'];
+nav4.textContent = siteContent['nav']['nav-item-4'];
+nav5.textContent = siteContent['nav']['nav-item-5'];
+nav6.textContent = siteContent['nav']['nav-item-6'];
+
+nav.forEach(font => font.classList.add("italic"));
+
+const logo = document.querySelector("#logo-img");
+logo.src = siteContent['images']['logo-img'];
+
 const title = document.querySelector("h1");
+title.textContent = siteContent['cta']['h1'];
 
 const button = document.querySelector("button");
+button.textContent = siteContent['cta']['button'];
 
 const ctaImage = document.querySelector("#cta-img");
+ctaImage.src = siteContent['images']['cta-img'];
 
-const features = document.querySelector("h4:nth-of-type(1)");
-const featureText = features.nextElementSibling;
 
-const about = document.querySelector("h4:nth-of-type(2)")
-const aboutText = about.nextElementSibling;
+//main content
+const h4 = document.querySelectorAll(".text-content h4");
+const mainContentP = document.querySelectorAll(".text-content p");
+const features = h4[0];
+const featuresText = mainContentP[0];
+features.textContent = siteContent['main-content']['features-h4'];
+featuresText.textContent = siteContent['main-content']['features-content'];
+
+const about = h4[1];
+const aboutText = mainContentP[1];
+about.textContent = siteContent['main-content']['about-h4'];
+aboutText.textContent = siteContent['main-content']['about-content'];
+
+const services = h4[2];
+const servicesText = mainContentP[2];
+services.textContent = siteContent['main-content']['services-h4'];
+servicesText.textContent = siteContent['main-content']['services-content']
+
+const product = h4[3];
+const productText =  mainContentP[3];
+product.textContent = siteContent['main-content']['product-h4'];
+productText.textContent = siteContent['main-content']['product-content'];
+
+const vision = h4[4];
+const visionText = mainContentP[4];
+vision.textContent = siteContent['main-content']['vision-h4'];
+visionText.textContent = siteContent['main-content']['vision-content']
+
+
 
 const middleImage = document.querySelector(".middle-img");
+middleImage.src = siteContent['images']['accent-img'];
 
-const services = document.querySelector("h4:nth-of-type(3)");
-const servicesText = services.nextElementSibling;
 
-const product = document.querySelector("h4:nth-of-type(4)");
-const productText = product.nextElementSibling;
-
-const vision = document.querySelector("h4:nth-of-type(5)");
-const visionText = nextElementSibling;
-
+//contact
 const contact = document.querySelector(".contact h4");
-const address = contact.nextElementSibling;
-const phone = address.nextElementSibling;
-const email = phone.nextElementSibling;
+contact.textContent = siteContent['contact']['contact-h4'];
 
+const contactInfo = document.querySelectorAll('.contact p')
+const address = contactInfo[0];
+address.textContent = siteContent['contact']['address'];
+
+const phone = contactInfo[1];
+phone.textContent = siteContent['contact']['phone'];
+
+const email = contactInfo[2];
+email.textContent = siteContent['contact']['email'];
+
+//footer
 const copyright = document.querySelector("footer a")
+copyright.textContent = siteContent['footer']['copyright']
+copyright.classList = 'bold';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
